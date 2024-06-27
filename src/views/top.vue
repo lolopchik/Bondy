c25e14e2f765bfab31f37f58ce0951a0fce24791
GameOver <147331169+lolopchik@users.noreply.github.com>
2024-06-27 22:43:17 +0400

    Update top.vue

--- src/views/top.vue
+++ src/views/top.vue
@@ -1,6 +1,6 @@
 <script>
 import axios from "axios"
-    axios.defaults.baseURL = "http://localhost:3000";
+    axios.defaults.baseURL = "https://serverbondy.onrender.com";
     
     export default {
         data(){
