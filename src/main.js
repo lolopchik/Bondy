e8dd224ddc2f59f08b4bf859eadcd4be12be579d
GameOver <147331169+lolopchik@users.noreply.github.com>
2024-06-29 13:25:19 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Запросы к серверу
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://serverbondy.onrender.com';
 
 // Работа с датой
 
